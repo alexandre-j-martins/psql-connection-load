eb129beb241778c2fe2932d6c481a0941c9bcac3
Alexandre J. Martins <alexandre@sigalei.com.br>
2025-12-03 18:32:06 -0300

    add thread

--- connect.py
+++ connect.py
@@ -1,6 +1,8 @@
 import os
 import psycopg
+import time
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from dotenv import load_dotenv
 
 
@@ -15,14 +17,30 @@ def connect():
     conn_string = "postgresql://{}:{}@{}:{}/{}".format(user, password, host, port, db)
     return psycopg.connect(conn_string)
 
-def select():
+def select(i):
     conn = connect()
     with conn.cursor() as cur:
         query = "SELECT first_name, last_name FROM artist"
         cur.execute(query)
         artists = cur.fetchall()
         for artist in artists:
-            print(artist[0], artist[1])
+            print("Thread", i, artist[0], artist[1])
 
 
-select()
+def main():
+    warm_up = 10
+    print("Initializing in {} seconds...".format(warm_up))
+    time.sleep(10)
+    start = time.time()
+    num_threads = 20   # number of simultaneous connections
+    
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = [executor.submit(select, i) for i in range(num_threads)]
+
+        for future in as_completed(futures):
+            print(future.result())
+
+    print("Total time:", time.time() - start)
+
+if __name__ == "__main__":
+    main()
